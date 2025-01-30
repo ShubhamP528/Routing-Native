87a2391d32215991ea793c4d5ad0add08b20d1eb
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-01-30 07:54:55 +0530

    basic Routing

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
  * @format
  */
 
-import TrackPlayer from 'react-native-track-player';
+// import TrackPlayer from 'react-native-track-player';
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+// import App from './src/App';
+// import App from './Routing/App';
+import App from './Routing2/App';
 import {name as appName} from './app.json';
-import {playbackService} from './musicPlayerServices';
+// import {playbackService} from './musicPlayerServices';
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => playbackService);
+// TrackPlayer.registerPlaybackService(() => playbackService);
