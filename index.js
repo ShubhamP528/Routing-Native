51d3f577325af683eee76165e8112526dfae8baf
Shubham Prajapati <137846021+ShubhamP528@users.noreply.github.com>
2025-01-30 07:57:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 // import TrackPlayer from 'react-native-track-player';
 import {AppRegistry} from 'react-native';
 // import App from './src/App';
-// import App from './Routing/App';
-import App from './Routing2/App';
+import App from './Routing/App';
+// import App from './Routing2/App';
 import {name as appName} from './app.json';
 // import {playbackService} from './musicPlayerServices';
 
